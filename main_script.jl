042a04db3a5c7a311ba892da50924a5c579f680d
RowanDurrant <rowang.durrant@gmail.com>
2022-05-17 14:35:36 +0100

    uploads

---
+++ main_script.jl
@@ -0,0 +1,18 @@
+using CSV
+using DataFrames
+
+main_list = CSV.read("main_list.csv", DataFrame)
+daily_list = CSV.read("daily_list.csv", DataFrame)
+main_list_updated = [main_list;daily_list]
+main_list_deduplicated = unique(main_list_updated, 1)
+CSV.write("main_list.csv", main_list_deduplicated)
+print("Done! \n \n")
+
+text = Vector{String}()
+push!(text, string("Sidor: \n\nNya ord: \n")) 
+for i=1:nrow(daily_list)
+    push!(text, string(daily_list[i, 1], " - ", daily_list[i,2], "\n"))
+end
+push!(text, string("\nSammanfattning:"))
+text_finished = join(text)
+print(text_finished)
