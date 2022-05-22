fcfe30bc14d0b9512e973b3044659a1235bc7f4f
RowanDurrant <rowang.durrant@gmail.com>
2022-05-22 15:44:43 +0100

    new words 22-05-22

--- main_script.jl
+++ main_script.jl
@@ -9,7 +9,7 @@ CSV.write("main_list.csv", main_list_deduplicated)
 print("Done! \n \n")
 
 text = Vector{String}()
-push!(text, string("Sidor: \n\nNya ord: \n")) 
+push!(text, string("Sidor: /561\nNya ord: \n")) 
 for i=1:nrow(daily_list)
     push!(text, string(daily_list[i, 1], " - ", daily_list[i,2], "\n"))
 end
