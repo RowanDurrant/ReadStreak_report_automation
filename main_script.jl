69a946c8feb78202cb9f7dc8f28989b32b39a2e8
RowanDurrant <58739837+RowanDurrant@users.noreply.github.com>
2023-03-10 14:28:39 +0000

    10.03.23 updates

--- main_script.jl
+++ main_script.jl
@@ -1,15 +1,17 @@
 using CSV
 using DataFrames
+using Dates
 
 main_list = CSV.read("main_list.csv", DataFrame)
 daily_list = CSV.read("daily_list.csv", DataFrame)
+
 main_list_updated = [main_list;daily_list]
 main_list_deduplicated = unique(main_list_updated, 1)
 CSV.write("main_list.csv", main_list_deduplicated)
 print("Done! \n \n")
 
 text = Vector{String}()
-push!(text, string("Sidor: /561\nNya ord: \n")) 
+push!(text, string("Streak X: [Paganinikontraktet] by [Lars Kepler]\n\nSidor: /561\nNya ord: \n")) 
 for i=1:nrow(daily_list)
     push!(text, string(daily_list[i, 1], " - ", daily_list[i,2], "\n"))
 end
